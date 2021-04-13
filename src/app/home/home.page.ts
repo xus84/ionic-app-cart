fe7e1089e83c512b591994dbde78f5b1b7ef2c35
garsan84 <jgrsan84@gmail.com>
2021-04-14 00:56:38 +0200

    template and some carts feautures

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
+import { CartService } from '../services/cart.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage  implements OnInit {
+  cart = [];
+  products = [];
+  cartItemCount: BehaviorSubject <number>;
+
+  constructor(private cartService: CartService, private modalCtrl: ModalController) {}
+
+  ngOnInit() {
+    this.products = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+    this.cartItemCount = this.cartService.getCartItemCount();
+  }
+
+  addToCart(product){
+    this.cartService.addProduct(product);
+  }
+
+  openCart(){
+
+  }
+}
