4679c695ebe9a0ae1f6102697e327ae600c02c57
garsan84 <jgrsan84@gmail.com>
2021-04-14 14:11:32 +0200

    cart modal functionalities

--- src/app/pages/cart-modal/cart-modal.page.ts
+++ src/app/pages/cart-modal/cart-modal.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { CartService } from 'src/app/services/cart.service';
+import { Product } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart-modal',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-modal.page.scss'],
 })
 export class CartModalPage implements OnInit {
+  cart: Product[] = [];
 
-  constructor() { }
+  constructor(private cartService: CartService, private modalCtrl: ModalController) { }
 
   ngOnInit() {
+    this.cart = this.cartService.getCart();
+  }
+
+  decreaseCartItem(product){
+    this.cartService.decreaseProduct(product);
+  }
+
+  increaseCartItem(product){
+    this.cartService.addProduct(product);
+  }
+
+  removeCartItem(product){
+    this.cartService.removeProduct(product);
+  }
+
+  getTotal(){
+    return this.cart.reduce((i, j) => i + j.price * j.amount, 0);
+  }
+
+  close() {
+    this.modalCtrl.dismiss();
   }
 
 }
